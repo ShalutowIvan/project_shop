88330a8fd01d144ce9b17227b92be8d0440bdbb7
ivan <ivan_shalutow@mail.ru>
2023-08-03 00:04:09 +0700

    ads

--- My_market/showcase/views.py
+++ My_market/showcase/views.py
@@ -4,9 +4,9 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 # Create your views here.
 
 def index(request):
-    data = {'группа 1': 'товар 1'}
-
-    return render(request, "showcase/home.html", {"data": 1111})
+    dat = {'группа 1': 'товар 1', 'группа 1': 'товар 2', 'группа 1': 'товар 3', 'группа 1': 'товар 4'}
+    # data = [1, 2, 3, 4]
+    return render(request, "showcase/home.html", {"data": dat})
 
 def basket(request):
     return render(request, "showcase/basket.html")
