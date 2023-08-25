ab724d7e3a2554bb0c6f4e041477b07d9d9e093a
Иван <ivan_shalutow@mail.ru>
2023-08-25 17:41:18 +0700

    asd

--- My_market/showcase/urls.py
+++ My_market/showcase/urls.py
@@ -3,9 +3,12 @@ from showcase.views import *
 
 
 urlpatterns = [#тут подключаем все ссылки которые будут идти после корневой ссылки из файла urls.py из папки с проектом
-	path('', start, name='start'),
+	path('', GoodsHome.as_view(), name='start'),
     path('basket/', basket, name='basket'),
-    path('group/<slug:group_slug>/', show_group, name='group'),
+    path('group/<slug:group_slug>/', GroupShow.as_view(), name='group'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('logout/', logout_user, name='logout'),
     # path('group/<str:name_product>/', add_in_basket, name='add_in_basket'),
     # path('product/<slug:product_slug>/', show_product, name='product')
     #для групп сделать
--- My_market/showcase/views.py
+++ My_market/showcase/views.py
@@ -1,54 +1,95 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth import logout, login
 
 from .models import *
 from .forms import *
+from .utils import *
 
 
+# def start(request):
+#     # gr = Group.objects.all()
+#     org = Organization.objects.all()
+#     gd = Goods.objects.all()
 
-def start(request):
-    # gr = Group.objects.all()
-    org = Organization.objects.all()
-    gd = Goods.objects.all()
+#     form = AddGoodForm()
 
-    form = AddGoodForm()
-
-    # tovar = Goods.objects.filter(name_product=name_product)
-    # t_basket = Goods_in_basket.objects.create(name_product=tovar.name_product, price=tovar.price, quantity=1, group=tovar.group)
-    # g_basket.save()
-    # print(tovar.name_product)
+#     # tovar = Goods.objects.filter(name_product=name_product)
+#     # t_basket = Goods_in_basket.objects.create(name_product=tovar.name_product, price=tovar.price, quantity=1, group=tovar.group)
+#     # g_basket.save()
+#     # print(tovar.name_product)
     
 
-    dat = {
-    # "gr": gr,
-    "org": org[0],
-    "gd": gd,
-    "form": form,
-    }
+#     dat = {
+#     # "gr": gr,
+#     "org": org[0],
+#     "gd": gd,
+#     "form": form,
+#     }
 
 
-    return render(request, "showcase/start.html", context=dat)
+#     return render(request, "showcase/start.html", context=dat)
+
+
+class GoodsHome(ListView):
+    model = Goods
+    template_name = 'showcase/start.html'
+    context_object_name = 'gd'
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        org = Organization.objects.all()
+        context['org'] = org[0]
+        context['form'] = AddGoodForm()
+        
+        
+        return context
 
 
 
-
-
-
-def show_group(request, group_slug): 
-    groups = Group.objects.filter(slug=group_slug)
-    goods = Goods.objects.filter(group_id=groups[0].id)
-    # gr = Group.objects.all()
-    # print(groups)
-    data = {
-    'goods': goods,    
-    # 'gr': gr,
-    'title': 'Товары',
-    }
+# def show_group(request, group_slug): 
+#     groups = Group.objects.filter(slug=group_slug)
+#     goods = Goods.objects.filter(group_id=groups[0].id)
+#     # gr = Group.objects.all()
+#     # print(groups)
+#     data = {
+#     'goods': goods,    
+#     # 'gr': gr,
+#     'title': 'Товары',
+#     }
    
 
-    return render(request, "showcase/good.html", context=data)
+#     return render(request, "showcase/good.html", context=data)
 
-   
+
+class GroupShow(ListView):
+    model = Goods
+    template_name = 'showcase/good.html'
+    context_object_name = 'goods'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Goods.objects.filter(group__slug=self.kwargs['group_slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Группа - ' + str(context['goods'][0].group)
+        org = Organization.objects.all()
+        context['org'] = org[0]
+        context['form'] = AddGoodForm()
+
+        return context
+
+
+
+
+
 
 # def add_in_basket(request, name_product):
 #     # if request.method == 'POST':
@@ -82,3 +123,36 @@ def basket(request):
     return render(request, "showcase/basket.html")
 
 #разобраться с добавлением товара в корзину
+
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'showcase/register.html'
+    success_url = reverse_lazy('login')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # c_def = self.get_user_context(title="Регистрация")
+        # return dict(list(context.items()) + list(c_def.items()))
+        context['title'] = "Регистрация"
+        return context
+
+
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'showcase/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Авторизация"
+        # c_def = self.get_user_context(title="Авторизация")
+        # return dict(list(context.items()) + list(c_def.items()))
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('start')
+
+def logout_user(request):#функция для выхода, чтобы выйти из аккаунта
+    logout(request)#эта функция вызывает стандартную функцию джанго для выхода пользователя.
+    return redirect('login')
