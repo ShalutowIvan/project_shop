21caa5b83044d85e92eefc3d85df15ddc0918772
Иван <ivan_shalutow@mail.ru>
2023-08-29 23:06:48 +0700

    asd

--- My_market/test.py
+++ My_market/test.py
@@ -1,2 +1,13 @@
-d = {1: 2, "asd": 33, "777": 666}
-print(d.items())
+class Person:
+    def __init__(fio, job, old, salary, year_job):
+        self.fio = fio
+        self.job = job
+        self.old = old
+        self.salary = salary
+        self.year_job = year_job
+        
+    def
+
+
+
+
